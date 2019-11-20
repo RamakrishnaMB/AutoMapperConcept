e05da5f0a0ed68d383ca6ad8bec72ff3f1e15ae3
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-11-20 12:47:55 +0530

    Buisnesslayer,Datalayer,Models class library added to github

---
+++ BuisnessLayer/Implementation/DashBoardFeeServices.cs
@@ -0,0 +1,24 @@
+﻿using BuisnessLayer.Interface;
+using DataLayer.Interface;
+using Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BuisnessLayer.Implementation
+{
+    public class DashBoardFeeServices : IDashBoardFeeServices
+    {
+        public IRepository<FeeDetails> _repository;
+
+        public DashBoardFeeServices(IRepository<FeeDetails> repository)
+        {
+            _repository = repository;
+        }
+        public List<FeeDetails> GetfeeDetails()
+        {
+            List<FeeDetails> lstFeeDetails = new List<FeeDetails>();
+            lstFeeDetails = _repository.ExecWithStoreProcedure<FeeDetails>("exec uspDashboard").ToList();
+            return lstFeeDetails;
+        }
+    }
+}
---
+++ BuisnessLayer/Interface/IDashBoardFeeServices.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System.Collections.Generic;
+
+
+namespace BuisnessLayer.Interface
+{
+    
+    public interface IDashBoardFeeServices
+    {
+        List<FeeDetails> GetfeeDetails();
+
+    }
+}
